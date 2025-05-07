1c9fe2b341a3505f1dc749db195ab2320ea37cbf
smill32076 <smill32076@gmail.com>
2025-05-07 14:52:00 +0000

    py檔設定

---
+++ lesson7/lesson7_1.py
@@ -0,0 +1,21 @@
+height:int = int(input("請輸入身高公分"))
+weight:int = int(input("請輸入體重公斤"))
+BMI:float = round(weight/pow(height/100,2),1)
+
+print(f"身高:{height}""cm")
+print(f"體重:{weight}""kg")
+print(f"BMI:{BMI}")
+
+if BMI < 18.5:
+    status = "體重過輕"
+elif BMI < 24:
+    status = "正常"
+elif BMI < 27:
+    status = "過重"
+elif BMI < 30:
+    status = "輕度肥胖"
+elif BMI <35:
+    status = "中度肥胖"
+else:
+    status = "重度肥胖"
+print(f"狀態:{status}")
