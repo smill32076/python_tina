b89945d295abcf431b554333dc5c343d14b70c31
smill32076 <smill32076@gmail.com>
2025-05-10 13:08:17 +0000

    lesson7

--- lesson7/lesson7_1.py
+++ lesson7/lesson7_1.py
@@ -1,21 +1,14 @@
-height:int = int(input("請輸入身高公分"))
-weight:int = int(input("請輸入體重公斤"))
-BMI:float = round(weight/pow(height/100,2),1)
+import tools
+def main():
+    height:int = int(input("請輸入身高公分"))
+    weight:int = int(input("請輸入體重公斤"))
+    BMI:float = tools.get_bmi(weight,height)
 
-print(f"身高:{height}""cm")
-print(f"體重:{weight}""kg")
-print(f"BMI:{BMI}")
+    print(f"身高:{height}""cm")
+    print(f"體重:{weight}""kg")
+    print(f"BMI:{BMI}")
+    status:str = tools.get_status(BMI)   
+    print(f"狀態為:{status}")
 
-if BMI < 18.5:
-    status = "體重過輕"
-elif BMI < 24:
-    status = "正常"
-elif BMI < 27:
-    status = "過重"
-elif BMI < 30:
-    status = "輕度肥胖"
-elif BMI <35:
-    status = "中度肥胖"
-else:
-    status = "重度肥胖"
-print(f"狀態:{status}")
+if __name__ == '__main__':
+    main()
